97b6f95ebcb3b668b37671f4a27d5e7b91391ef2
Markus Gollmann <markus.gollmann@edu.fh-joanneum.at>
2021-12-18 11:58:19 +0000

    Start Web App

---
+++ web/html/ccm/ccm_info_get.js
@@ -0,0 +1,1 @@
+message({type:"ccm_info_get_ack",from:542113792,from_handle:2252,to_handle:433491,data:{ret:{code:"",sub:"",reason:"",desc:""},type:"IPC",sn:"1jfiegbrcrfka",nick:"MasterThesis",ver:"v5.3.1.2003161406",spv:"v1"}});
